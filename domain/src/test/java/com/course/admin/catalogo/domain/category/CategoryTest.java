ba8f3bbb59f69bbbfbd85524e93c4156083a8c93
Denis <denis.william@seasolutions.io>
2024-04-25 20:33:30 -0400

    chore: created Category and CategoryTest classes with minimal aspects to represent their scope and functionality.

---
+++ domain/src/test/java/com/course/admin/catalogo/domain/category/CategoryTest.java
@@ -0,0 +1,26 @@
+package com.course.admin.catalogo.domain.category;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+public class CategoryTest {
+    @Test
+    public void givenAValidParams_whenCallNewCategory_thenInstantiateACategory() {
+        final var expectedName = "Filmes";
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedIsActive = true;
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+
+        assertNotNull(actualCategory);
+        assertNotNull(actualCategory.getId());
+        assertEquals(expectedName, actualCategory.getName());
+        assertEquals(expectedDescription, actualCategory.getDescription());
+        assertEquals(expectedIsActive, actualCategory.isActive());
+        assertNotNull(actualCategory.getCreatedAt());
+        assertNotNull(actualCategory.getUpdatedAt());
+        assertNull(actualCategory.getDeletedAt());
+    }
+}
