916722c48f37315be5d9bfe3d659fe646847042b
Denis <denis.william@seasolutions.io>
2024-04-25 21:09:50 -0400

    test: add new test to check when validate is call with null name if DomainException will be thrown and check the expected message is given.

--- domain/src/test/java/com/course/admin/catalogo/domain/category/CategoryTest.java
+++ domain/src/test/java/com/course/admin/catalogo/domain/category/CategoryTest.java
@@ -1,5 +1,6 @@
 package com.course.admin.catalogo.domain.category;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
@@ -21,5 +22,19 @@ public class CategoryTest {
         assertNotNull(actualCategory.getCreatedAt());
         assertNotNull(actualCategory.getUpdatedAt());
         assertNull(actualCategory.getDeletedAt());
+    }
+    @Test
+    public void givenAnInvalidNullName_whenCallNewCategoryAndValidate_thenShouldReceiveError() {
+        final String expectedName = null;
+        final var expectedDescription = "A categoria mais assistida";
+        final var expectedErrorMessage = "'name' should not be null";
+        final var expectedErrorCount = 1;
+        final var expectedIsActive = true;
+        final var actualCategory = Category.newCategory(expectedName, expectedDescription, expectedIsActive);
+        final var actualException =
+                assertThrows(DomainException.class, () -> actualCategory.validate());
+        assertEquals(expectedErrorCount, actualException.getErrors().size());
+        assertEquals(expectedErrorMessage, actualException.getErrors().get(0));
+
     }
 }
