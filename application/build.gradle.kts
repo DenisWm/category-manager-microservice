9842c54f5967d11727652d53b86defd3ecf38203
Denis <denis.william@seasolutions.io>
2024-04-30 18:58:19 -0400

    config: adding new dependency varv object-functional library.

--- application/build.gradle.kts
+++ application/build.gradle.kts
@@ -11,6 +11,7 @@ repositories {
 
 dependencies {
     implementation(project(":domain"))
+    implementation("io.vavr:vavr:0.10.4")
     testImplementation(platform("org.junit:junit-bom:5.9.1"))
     testImplementation("org.junit.jupiter:junit-jupiter")
     testImplementation("org.mockito:mockito-junit-jupiter:4.5.1")
