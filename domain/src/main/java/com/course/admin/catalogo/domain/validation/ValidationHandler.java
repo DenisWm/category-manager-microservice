bd38d5d8cca64550607f2d0eb9943513ddaa194d
Denis <denis.william@seasolutions.io>
2024-04-30 18:58:31 -0400

    chore: new helper method.

--- domain/src/main/java/com/course/admin/catalogo/domain/validation/ValidationHandler.java
+++ domain/src/main/java/com/course/admin/catalogo/domain/validation/ValidationHandler.java
@@ -10,6 +10,14 @@ public interface ValidationHandler {
     default boolean hasErrors() {
         return getErrors() != null && !getErrors().isEmpty();
     }
+    default Error firstError() {
+        if (getErrors() != null && !getErrors().isEmpty()) {
+            return getErrors().get(0);
+
+        } else {
+            return null;
+        }
+    }
 
     List<Error> getErrors();
     interface Validation{
