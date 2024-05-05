35577fc4ed61491015f5f3fef51e287f3c85ef2f
Denis <denis.william@seasolutions.io>
2024-05-04 23:39:11 -0400

    chore: added map method to return a different type.

--- domain/src/main/java/com/course/admin/catalogo/domain/pagination/Pagination.java
+++ domain/src/main/java/com/course/admin/catalogo/domain/pagination/Pagination.java
@@ -1,6 +1,7 @@
 package com.course.admin.catalogo.domain.pagination;
 
 import java.util.List;
+import java.util.function.Function;
 
 public record Pagination<T>(
         int currentPage,
@@ -8,4 +9,11 @@ public record Pagination<T>(
         long total,
         List<T> items
 ) {
+    public <R> Pagination<R> map(final Function<T, R> mapper) {
+        final List<R> aNewList = this.items.stream()
+                .map(mapper)
+                .toList();
+
+        return new Pagination<>(currentPage(), perPage(), total(), aNewList);
+    }
 }
