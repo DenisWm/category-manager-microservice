33ff8ac9538a2c1fcef47d013ce95110ea339533
Denis <denis.william@seasolutions.io>
2024-05-06 22:11:17 -0400

    config: spring configuration files

--- infrastructure/src/main/java/com/course/admin/catalogo/infrastructure/Main.java
+++ infrastructure/src/main/java/com/course/admin/catalogo/infrastructure/Main.java
@@ -4,10 +4,12 @@ import com.course.admin.catalogo.application.UseCase;
 import com.course.admin.catalogo.infrastructure.configuration.WebServerConfig;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.core.env.AbstractEnvironment;
 
 @SpringBootApplication
 public class Main {
     public static void main(String[] args) {
+        System.setProperty(AbstractEnvironment.DEFAULT_PROFILES_PROPERTY_NAME, "development");
         SpringApplication.run(WebServerConfig.class, args);
     }
 }
