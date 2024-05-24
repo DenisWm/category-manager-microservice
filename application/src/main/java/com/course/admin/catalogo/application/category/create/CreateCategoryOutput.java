642762e58506ce3ef270b6d6a08db95900055944
denis <denis.william@seasolutions.io>
2024-05-24 18:50:34 -0400

    chore: add new method.

--- application/src/main/java/com/course/admin/catalogo/application/category/create/CreateCategoryOutput.java
+++ application/src/main/java/com/course/admin/catalogo/application/category/create/CreateCategoryOutput.java
@@ -1,12 +1,15 @@
 package com.course.admin.catalogo.application.category.create;
 
 import com.course.admin.catalogo.domain.category.Category;
-import com.course.admin.catalogo.domain.category.CategoryID;
 
 public record CreateCategoryOutput(
-        CategoryID id
+        String id
 ) {
     public static CreateCategoryOutput from (final Category aCategory){
-        return new CreateCategoryOutput(aCategory.getId());
+        return new CreateCategoryOutput(aCategory.getId().getValue());
+    }
+
+    public static CreateCategoryOutput from (final String anId){
+        return new CreateCategoryOutput(anId);
     }
 }
