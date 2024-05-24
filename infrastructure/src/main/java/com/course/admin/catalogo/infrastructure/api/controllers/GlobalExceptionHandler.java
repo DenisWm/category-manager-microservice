ba596ac43b5219e74d6a3c16052d94431a2d2865
denis <denis.william@seasolutions.io>
2024-05-24 18:51:35 -0400

    chore: create RestControllerAdvice to handle exceptions

---
+++ infrastructure/src/main/java/com/course/admin/catalogo/infrastructure/api/controllers/GlobalExceptionHandler.java
@@ -0,0 +1,30 @@
+package com.course.admin.catalogo.infrastructure.api.controllers;
+
+import com.course.admin.catalogo.domain.exceptions.DomainException;
+import com.course.admin.catalogo.domain.exceptions.NotFoundException;
+import com.course.admin.catalogo.domain.validation.Error;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.List;
+
+@RestControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(DomainException.class)
+    public ResponseEntity<?> handleDomainException(DomainException e) {
+        return ResponseEntity.unprocessableEntity().body(ApiError.from(e));
+    }
+    @ExceptionHandler(NotFoundException.class)
+    public ResponseEntity<?> handleNotFoundException(NotFoundException e) {
+        return ResponseEntity.status(HttpStatus.NOT_FOUND).body(ApiError.from(e));
+    }
+
+    record ApiError(String message, List<Error> errors) {
+        static ApiError from(DomainException ex) {
+            return new ApiError(ex.getMessage(), ex.getErrors());
+        }
+    }
+}
