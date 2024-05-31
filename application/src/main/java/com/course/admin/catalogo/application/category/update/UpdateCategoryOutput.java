c0ef9e935ada19ee2269ebf99091767c0b7e560c
denis <denis.william@seasolutions.io>
2024-05-31 10:10:50 -0400

    chore: making requests and outputs records better.

--- application/src/main/java/com/course/admin/catalogo/application/category/update/UpdateCategoryOutput.java
+++ application/src/main/java/com/course/admin/catalogo/application/category/update/UpdateCategoryOutput.java
@@ -1,13 +1,15 @@
 package com.course.admin.catalogo.application.category.update;
 
 import com.course.admin.catalogo.domain.category.Category;
-import com.course.admin.catalogo.domain.category.CategoryID;
 
 public record UpdateCategoryOutput(
-        CategoryID id
+        String id
 ) {
 
+    public static UpdateCategoryOutput from(final String anId) {
+        return new UpdateCategoryOutput(anId);
+    }
     public static UpdateCategoryOutput from(final Category aCategory) {
-        return new UpdateCategoryOutput(aCategory.getId());
+        return new UpdateCategoryOutput(aCategory.getId().getValue());
     }
 }
--- infrastructure/src/main/java/com/course/admin/catalogo/infrastructure/category/models/CreateCategoryRequest.java
+++ infrastructure/src/main/java/com/course/admin/catalogo/infrastructure/category/models/CreateCategoryRequest.java
@@ -2,7 +2,7 @@ package com.course.admin.catalogo.infrastructure.category.models;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public record CreateCategoryApiInput(
+public record CreateCategoryRequest(
         @JsonProperty("name") String name,
         @JsonProperty("description") String description,
         @JsonProperty("is_active") Boolean active
