3431de3ef51480c2ecefb1d451c0c34ba1d495d7
denis <denis.william@seasolutions.io>
2024-06-02 21:53:42 -0400

    chore: json object mapper.

--- infrastructure/src/main/java/com/course/admin/catalogo/infrastructure/configuration/ObjectMapperConfig.java
+++ infrastructure/src/main/java/com/course/admin/catalogo/infrastructure/configuration/ObjectMapperConfig.java
@@ -1,7 +1,14 @@
 package com.course.admin.catalogo.infrastructure.configuration;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class ObjectMapperConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return Json.mapper();
+    }
 }
