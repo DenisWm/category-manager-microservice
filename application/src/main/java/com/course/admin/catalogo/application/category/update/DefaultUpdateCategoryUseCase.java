08c7755d3734c0d11304faf25698751d6163fda6
Denis <denis.william@seasolutions.io>
2024-06-16 20:57:35 -0300

    chore(Category): refactoring not found

--- application/src/main/java/com/course/admin/catalogo/application/category/update/DefaultUpdateCategoryUseCase.java
+++ application/src/main/java/com/course/admin/catalogo/application/category/update/DefaultUpdateCategoryUseCase.java
@@ -1,10 +1,12 @@
 package com.course.admin.catalogo.application.category.update;
 
+import com.course.admin.catalogo.application.utils.IDNotFoundUtils;
 import com.course.admin.catalogo.domain.category.Category;
 import com.course.admin.catalogo.domain.category.CategoryGateway;
 import com.course.admin.catalogo.domain.category.CategoryID;
 import com.course.admin.catalogo.domain.exceptions.DomainException;
 import com.course.admin.catalogo.domain.exceptions.NotFoundException;
+import com.course.admin.catalogo.domain.genre.Genre;
 import com.course.admin.catalogo.domain.validation.handler.Notification;
 import io.vavr.control.Either;
 
@@ -29,7 +31,7 @@ public class DefaultUpdateCategoryUseCase extends UpdateCategoryUseCase{
         final var aDescription = aCommand.description();
         final var isActive = aCommand.isActive();
 
-        final var aCategory = this.categoryGateway.findById(anId).orElseThrow(notFound(anId));
+        final var aCategory = this.categoryGateway.findById(anId).orElseThrow(IDNotFoundUtils.notFound(anId, Category.class));
 
         final var notification = Notification.create();
 
@@ -44,7 +46,4 @@ public class DefaultUpdateCategoryUseCase extends UpdateCategoryUseCase{
                 .bimap(Notification::create, UpdateCategoryOutput::from);
     }
 
-    private static Supplier<DomainException> notFound(final CategoryID anId) {
-        return () -> NotFoundException.with(Category.class, anId);
-    }
 }
