ffe5417d3a4ea53b44654c07f62248c75cc267e4
Denis <denis.william@seasolutions.io>
2024-06-16 20:55:55 -0300

    chore(Category): adding existsById in gateway

--- infrastructure/src/main/java/com/course/admin/catalogo/infrastructure/category/CategoryMySQLGateway.java
+++ infrastructure/src/main/java/com/course/admin/catalogo/infrastructure/category/CategoryMySQLGateway.java
@@ -12,6 +12,9 @@ import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.domain.Specification;
 import org.springframework.stereotype.Service;
 
+import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
 import java.util.Optional;
 
 import static com.course.admin.catalogo.infrastructure.utils.SpecificationUtils.like;
@@ -74,6 +77,12 @@ public class CategoryMySQLGateway implements CategoryGateway {
                 pageResult.getTotalElements(),
                 pageResult.map(CategoryJpaEntity::toAggregate).toList()
         );
+    }
+
+    @Override
+    public List<CategoryID> existsByIds(Iterable<CategoryID> ids) {
+        //TODO: Implementar quando chegar na camada de infraestrutura de Genre
+        return Collections.emptyList();
     }
 
     private Category save(final Category aCategory) {
