fdaca5819ea22263ce5d7467564a5c6c7818e0ad
DenisWm <denismamoni@hotmail.com>
2025-01-23 14:27:13 -0300

    FINAL

--- sandbox/run.sh
+++ sandbox/run.sh
@@ -1,9 +1,16 @@
 docker network create adm_videos_services
+docker network create elastic
 
+sudo chown root app/filebeat/filebeat.docker.yml
 mkdir -m 777 .docker
+mkdir -m 777 .docker/es01
 mkdir -m 777 .docker/keycloak
+mkdir -m 777 .docker/filebeat
 
 docker compose -f services/docker-compose.yml up -d
+docker compose -f elk/docker-compose.yml up -d
+docker compose -f app/docker-compose.yml up -d
+
 echo  "Inicializando os containers..."
 sleep 20
 
